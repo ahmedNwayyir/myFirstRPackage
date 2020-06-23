84fc3ba61d5a1cb7e13d32ee9e9de9756a1a02a3
Ahmed Nwayyir <nwayyir.ahmed@gmail.com>
2020-06-23 15:20:54 +0200

    - added a line at the bottom of DESCRIPTION file - changed the hyperlink for euclidean()

--- R/euclidean.R
+++ R/euclidean.R
@@ -2,22 +2,25 @@
 #'
 #' @param a A number.
 #' @param b A number.
-#' @return the largest number that divides \code{a} and \code{b}.
+#' @return The largest number that divides \code{a} and \code{b}.
 #' @examples
 #' euclidean(123612, 13892347912)
 #' euclidean(100, 1000)
 #'
 #' @references
-#' \href{https://en.wikipedia.org/wiki/Euclidean_algorithm}{https://en.wikipedia.org/wiki/Dijkstra\%27s_algorithm}
+#' \href{https://en.wikipedia.org/wiki/Euclidean_algorithm}{https://en.wikipedia.org/wiki/Euclidean_algorithm}
 #' @export
 
 euclidean <- function(a, b){
+
   stopifnot(is.numeric(a), is.numeric(b), length(a) == 1, length(b) == 1)
+
   while(b != 0){
     t <- b
     b <- a %% b
     a <- t
   }
+
   return(a)
 }
 
